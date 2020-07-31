1e5686ab741db92d50ecb61ca562587db20e4657
allanyeznaian <allanyeznaian1@gmail.com>
2020-07-31 14:58:40 -0700

    SDFDS:FKJ

---
+++ WebApplication3/App_Start/StartupAuth.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Security.Principal;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace WebApplication3
+{
+
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = EnsureTrailingSlash(ConfigurationManager.AppSettings["ida:AADInstance"]);
+        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        private static string asdf = ConfigurationManager.AppSettings["ida:Domain"];
+        
+        string authority = aadInstance + tenantId;
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+           var test = ConfigurationManager.AppSettings;
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            //var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+            //login as allan
+            string a = System.Threading.Tasks.Task.FromResult(0).ToString();
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        AuthenticationFailed = (context) =>
+                        {
+                            return System.Threading.Tasks.Task.FromResult(0);
+                        }
+                    }
+                }
+                );
+
+
+            // This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+
+        private static string EnsureTrailingSlash(string value)
+        {
+            if (value == null)
+            {
+                value = string.Empty;
+            }
+
+            if (!value.EndsWith("/", StringComparison.Ordinal))
+            {
+                return value + "/";
+            }
+
+            return value;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
